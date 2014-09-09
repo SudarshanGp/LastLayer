d4aaf7223384e6610dd3a94f61c33720135cf40d
Abhishek Modi <abhishek.modi95@gmail.com>
2014-09-09 18:01:30 -0500

    Added todos

--- mhacks/selector.py
+++ mhacks/selector.py
@@ -1,7 +1,7 @@
 import time
 from twilio.rest import TwilioRestClient 
  
-# put your own credentials here 
+
 ACCOUNT_SID = "AC5e89975323b46e4d820390f8ad591339" 
 AUTH_TOKEN = "836c46b0edb008a44a980fde1ce77d7a" 
 report = open("C:\\Users\\Abhishek\\Desktop\\MHacksIV\\report.txt", "w+")
@@ -14,6 +14,10 @@ time.sleep(1)
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
 
+#Haven't decided on authentication mehtod as yet
+#this file should only invoke methods to send auth prompts
+
+
 def sendMessage_simp():
 	report.write("message sent to ")
 	report.write(number)
@@ -22,8 +26,15 @@ def sendMessage_simp():
 	client.messages.create(
 		to=number, 
 		from_="+16092566342", 
-		body="Last Layer alert.\nThis is a courtesy message. Please reply Y to authenticate.",  
+		body="MHacks IV - Last Layer alert.\nA request to be access has been made by your account. Please reply Y to authenticate if this is you.",  
 	)
+
+
+def getMethod():
+	print "This method is supposed to find this user\'s preffered\n"
+	print "choice(s) of authentication\n"
+	print "this is still to be implemented\n"
+
 
 def getID():
 	scan = open("C:\\Users\\Abhishek\\Desktop\\MHacksIV\\scan.json", "r")
@@ -49,10 +60,17 @@ def getID():
 	report.write("end of getID")
 
 
+
 def main():
 	getID()
+	getMethod()
 	if (found != 0):
 		sendMessage_simp()
+	#the if found statement will most likely be removed
+	#from this function. It should be shifted to the getMethod function
+	#getmethon can iteratively go through the multiple
+	#set preferences and invoke them as required
+
 
 if __name__ == "__main__":
 	main()
